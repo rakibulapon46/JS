bad0499cde18f62443ce83bd0619393164577549
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-02-07 01:33:19 +0600

    type conversion

---
+++ type conversion/index.js
@@ -0,0 +1,48 @@
+//! type conversion = change the datatype of a value to another (strings, numbers, booleans)
+
+/*
+let age =  window.prompt('How old are you?');
+age = Number(age);  // convert string to number
+age+=1;
+console.log(age,  typeof age);
+*/
+
+
+/*
+let name = "Apon";
+console.log(name, typeof name);
+
+let age = 18;
+console.log(age, typeof age);
+
+let college = false;
+console.log(college, typeof college);
+
+let university = true;
+console.log(university, typeof university);
+*/
+
+
+// let x = 'pizza';
+// let y = 'pizza';
+// let z = 'pizza';
+
+// let x = '0';
+// let y = '0';
+// let z = '0';
+
+// let x = '';
+// let y = '';
+// let z = '';
+
+let x;
+let y;
+let z;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
