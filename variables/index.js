83ced310ae6c7b96277c0a8ee5ebab83cff071cd
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-02-06 19:16:09 +0600

    variables

---
+++ variables/index.js
@@ -0,0 +1,24 @@
+// variables
+let firstName = "Rakibul";
+console.log("🚀 ~ file: index.js:3 ~ firstName:", firstName);
+
+const lastName = "Apon";
+console.log("🚀 ~ file: index.js:5 ~ lastName:", lastName);
+
+// window.alert(`My name is ${firstName} ${lastName}`);
+
+
+//boolean
+let online = true;
+let offline = false;
+console.log(`he is online ${online}`);
+console.log(`she is online ${offline}`);
+
+
+let fullName = 'Rakibul Apon';
+let age = 18;
+let isStudent = true;
+
+document.getElementById('p1').textContent = `My name is ${fullName}.`;
+document.getElementById('p2').textContent = `I'm ${age} years old.`;
+document.getElementById('p3').textContent = `Enrolled: ${isStudent}.`;
