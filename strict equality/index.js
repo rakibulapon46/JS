a41678b77b6e87721061277b3155ad1f16746536
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-02-16 04:28:14 +0600

    strict equality

---
+++ strict equality/index.js
@@ -0,0 +1,41 @@
+// = assignment operator
+// == comparison operator (compare if values are equal)
+// === strict equality operator (compare if values & datatype are equal)
+// != inequality operator (compare if values are equal)
+// !== strict inequality operator (compare if values & datatype are equal)
+
+
+
+const PI = 3.1416
+
+
+// (==)
+if (PI == "3.1416") {
+    console.log("This is PI");
+} else {
+    console.log("This is not PI");
+}
+
+
+// (===)
+if (PI === "3.1416") {
+    console.log("This is PI");
+} else {
+    console.log("This is not PI");
+}
+
+
+// (!=)
+if (PI != "3.1416") {
+    console.log("This is NOT PI");
+} else {
+    console.log("This is PI");
+}
+
+
+// (!==)
+if (PI !== "3.1416") {
+    console.log("This is NOT PI");
+} else {
+    console.log("This is PI");
+}
