2b8ab87dc68799c723188b43b5568546c67b1135
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-02-16 23:54:05 +0600

    for loops

--- for loops/index.js
+++ for loops/index.js
@@ -1,3 +1,16 @@
 // for loops = repeat some code a LIMITED amount of times
 
 
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i);
+// }
+
+
+for (let i = 1; i <= 20; i++) {
+    if (i == 13) {
+        // continue;
+        break;
+    } else {
+        console.log(i);
+    }
+}
