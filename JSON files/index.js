d44f33d1fe5d1642c64c76bd763019b1d605d44a
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-03-08 23:20:02 +0600

    index js

---
+++ JSON files/index.js
@@ -0,0 +1,9 @@
+// JSON = (JavaScript Object Notation) data-interchange format
+//        Used for exchanging data between a server and a ueb application
+//        JSON files {key:value} OR [value, value2, value3]
+
+//        JSON.stringify() = converts a JS object to a JSON string
+//        JSON.parse() = converts a JSON string to a JS object
+
+
+
