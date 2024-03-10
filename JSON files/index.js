a0c486a6f2eb7015f1108d064c4e6355eea77525
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-03-10 21:28:44 +0600

    JSON files

--- JSON files/index.js
+++ JSON files/index.js
@@ -5,5 +5,59 @@
 //        JSON.stringify() = converts a JS object to a JSON string
 //        JSON.parse() = converts a JSON string to a JS object
 
+//! JSON.stringify()
+/*
+const names = ["Rakibul", "Apon", "Antor", "jubayer"];
 
+const person = {
+  name: "Rakibul",
+  age: 21,
+  isEmployed: true,
+  hobbies: ["riding", "gaming", "playing"],
+};
 
+const people = [
+  {
+    name: "Rakibul",
+    age: 21,
+    isEmployed: true,
+  },
+  {
+    name: "Apon",
+    age: 18,
+    isEmployed: false,
+  },
+  {
+    name: "Antor",
+    age: 4,
+    isEmployed: false,
+  },
+  {
+    name: "Rakibul",
+    age: 8,
+    isEmployed: true,
+  },
+];
+
+const jsonString = JSON.stringify(people);
+console.log(jsonString);
+*/
+
+//! JSON.parse()
+/*
+const jsonName = `["Rakibul", "Apon", "Antor", "jubayer"]`;
+const jsonPerson = `{"name": "Rakibul", "age": 21, "isEmployed": true, "hobbies": ["riding", "gaming", "playing"]}`;
+
+const jsonPeople = `[{  "name": "Rakibul",  "age": 21,  "isEmployed": true},{  "name": "Apon",  "age": 18,  "isEmployed": false},{  "name": "Antor",  "age": 4,  "isEmployed": false},{  "name": "Rakibul",  "age": 8,  "isEmployed": true}
+  ]
+  `;
+
+  const parseData = JSON.parse(jsonPeople);
+  console.log(parseData);
+*/
+
+//! fetch
+fetch("people.json")
+  .then((response) => response.json())
+  .then((values) => values.forEach((value) => console.log(value.name)))
+  .catch((error) => console.error(error));
